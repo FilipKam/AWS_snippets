04f3481fcb22d832a7141d4289c9ea1cafc9dd6e
Filip Kamenar <89387244+FilipKam@users.noreply.github.com>
2023-02-01 21:35:49 +0100

    Update lambda_function.py

--- Lamda/jwt_operations/lambda_function.py
+++ Lamda/jwt_operations/lambda_function.py
@@ -2,15 +2,24 @@ import json
 import jwt
 
 def lambda_handler(event, context):
-    jwt_encoded = event['headers']['x-amzn-oidc-accesstoken'] # get jwt from header
-    jwt_decoded = jwt.decode(jwt_encoded, options={"verify_signature": False}) # decode jwt
+    access_token_jwt_encoded = event['headers']['x-amzn-oidc-accesstoken'] # get jwt from header
+    access_token_jwt_decoded = jwt.decode(access_token_jwt_encoded, options={"verify_signature": False}) # decode jwt
     
-    output_json =  {"Original-Event":"","Encoded-JWT":"", "Decoded-JWT":""} # create a json object
+    identity_token_jwt_encoded = event['headers']['x-amzn-oidc-data'] # get jwt from header
+    identity_token_jwt_decoded = jwt.decode(identity_token_jwt_encoded, options={"verify_signature": False}) # decode jwt
+    
+    # create a json object
+    output_json =  {"Original-Event":"","Access-token-encoded-JWT":"", "Access-token-decoded-JWT":"", "Identity-token-encoded-JWT":"", "Identity-token-decoded-JWT":""}
 
     output_json['Original-Event'] = event # add original event to json object
-    output_json['Encoded-JWT'] = jwt_encoded # add encoded jwt to json object
-    output_json['Decoded-JWT'] = jwt_decoded # add decoded jwt to json object
+    output_json['Access-token-encoded-JWT'] = access_token_jwt_encoded # add encoded jwt to json object
+    output_json['Access-token-decoded-JWT'] = access_token_jwt_decoded # add decoded jwt to json object
+    
+    output_json['Identity-token-encoded-JWT'] = identity_token_jwt_encoded # add encoded jwt to json object
+    output_json['Identity-token-decoded-JWT'] = identity_token_jwt_decoded # add decoded jwt to json object
+    
 
+    
     return {
         'headers':{
             "Content-Type":"application/json"
